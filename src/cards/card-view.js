7955d2a7082f410c2e0376eae544e090086e72ed
Daniel Panero <panero.daniel@gmail.com>
2018-06-18 15:28:07 +0200

    Removed old fetching system in order to put everthing the store (re)

--- src/cards/card-view.js
+++ src/cards/card-view.js
@@ -5,7 +5,7 @@ import {installRouter} from 'pwa-helpers/router';
 import {connect} from 'pwa-helpers/connect-mixin'
 import store from '../redux/store'
 import {navigate} from '../redux/actions/router-actions'
-import {cardsLoaded, cardsAdded, cardsSaved} from '../redux/actions/card-actions'
+import {cardsFetch, cardsAdded, cardsSaved} from '../redux/actions/card-actions'
 
 import {UUID} from '../miscellaneous/random'
 
@@ -18,9 +18,6 @@ class CardView extends connect(store)(LitElement) {
 
   static get properties() { return { edit: Boolean, add: Boolean, data: Object}}
 
-  _shouldRender(props, changedProps, prevProps){
-  }
-
   // Redux
   _stateChanged(state) {
     ({edit: this.edit, add: this.add} = state.router)
@@ -29,50 +26,54 @@ class CardView extends connect(store)(LitElement) {
 
   // Router
   _firstRendered() {
-    installRouter((location) => this._locationChanged(location));
+    //Initialize Cards
+    store.dispatch(cardsFetch())
+
+    installRouter(location => {
+      store.dispatch(navigate(location.pathname))
+      this._locationChanged(location)
+    })
   }
 
   async _locationChanged(location) {
-    if(location.pathname == '/add/'){
-      await import('../components/card-modal.js');
-      await import('../components/card-input.js'); 
+    switch(location.pathname){
+      case '/':
+        if(!Object.is(this.shadowRoot.querySelector('card-list').cards, this.data.cards)){
+          store.dispatch(cardsSaved(this.shadowRoot.querySelector('card-list').cards))
+        }
+
+      case '/add/':
+        await import('../components/card-modal.js')
+        await import('../components/card-input.js')
+
+      case '/remove/':
+        await import('../components/card-modal.js');
     }
-    if(location.pathname == '/remove/'){
-      await import('../components/card-modal.js');
-    }
-    store.dispatch(navigate(location.pathname))
   }
 
-  async RequestCards() {
-    if(Object.keys(this.data).length === 0){
-      let response = await fetch('/data/card-tiny.json')
-      let data = await response.json()
-      await store.dispatch(cardsLoaded(data))
-    }
+  //Handlers
+
+  addCard() {
+    store.dispatch(cardsAdded({
+      "id": UUID(),
+      "text": this.shadowRoot.querySelectorAll('card-input')[0].text,
+      "definition": this.shadowRoot.querySelectorAll('card-input')[1].text
+    }))
+    this.shadowRoot.querySelectorAll('card-input').forEach(item => item.empty())
+  }
+
+  //Templates
+
+get RequestCards() {
     return html `
       <h2 class="title">${this.data.title}</h2>
       ${this.edit ? this.EditButtonsBar : this.DefaultButtonsBar}
       <card-list edit="${this.edit}" cards="${this.data.cards}"></card-list>`
   }
 
-  addCard() {
-    store.dispatch(cardsAdded([{
-      "id": UUID(),
-      "text": this.shadowRoot.querySelectorAll('card-input')[0].text,
-      "definition": this.shadowRoot.querySelectorAll('card-input')[1].text
-    }]))
-
-    this.shadowRoot.querySelectorAll('card-input')[0].empty()
-    this.shadowRoot.querySelectorAll('card-input')[1].empty()
-  }
-
-  saveCard(){
-    store.dispatch(cardsSaved(this.shadowRoot.querySelector('card-list').cards))
-  }
-
   get EditButtonsBar() {
     return html `
-  <card-button href="/" on-click="${e => this.saveCard()}">SAVE</card-button>
+  <card-button href="/">SAVE</card-button>
   <card-button class="inverted" href="/add/">ADD</card-button>
   <card-button class="inverted" href="/remove/">REMOVE/MOVE</card-button>
   `
@@ -85,8 +86,7 @@ class CardView extends connect(store)(LitElement) {
   `
   }
 
-  _render({edit = false, add = false, data = {}}) {
-    console.log('ehehe')
+  _render({edit, add, data}) {
     return html`
       <style>
         .title {
@@ -98,14 +98,14 @@ class CardView extends connect(store)(LitElement) {
         }
 
         .visible {
-          --modal-visibility: visible
+          --modal-display: block
         }
 
         card-modal {
-          visibility: hidden
+          display: var(--modal-display, none)
         }
       </style>
-      ${until(this.RequestCards(), html`<spinner-round></spinner-round>`)}
+      ${this.data.isLoading ? html`<spinner-round></spinner-round>` : this.RequestCards}
       <card-modal class$="${add ? `visible`: ``}" title="ADD CARDS:" href="/edit/">
       <div slot="content">
         <p>Word:</p>
