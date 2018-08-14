f0a5868f84e8288db5d5bf9c7af45a0a37dc6132
Daniel Panero <panero.daniel@gmail.com>
2018-08-14 11:05:52 +0200

    Added hapi as server

---
+++ server.js
@@ -0,0 +1,72 @@
+'use strict'
+
+const Hapi = require('hapi')
+const Path = require('path')
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+    routes: {
+        files: {
+            relativeTo: Path.join(__dirname, 'build/development/')
+        }
+    }
+})
+
+const init = async () => {
+
+    await server.register(require('inert'))
+
+    await server.register(require('bell'))
+
+    server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            file: 'index.html'
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/src/{param*}',
+        handler: {
+            directory: {
+                path: './src/',
+            }
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/data/{param*}',
+        handler: {
+            directory: {
+                path: './data/',
+            }
+        }
+    });
+
+    
+    server.route({
+        method: 'GET',
+        path: '/node_modules/{param*}',
+        handler: {
+            directory: {
+                path: './node_modules/',
+            }
+        }
+    });
+
+
+    await server.start()
+    console.log(`Server running at: ${server.info.uri}`)
+}
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err)
+    process.exit(1)
+})
+
+init()
