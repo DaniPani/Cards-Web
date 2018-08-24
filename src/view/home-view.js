54a8137ef4537fb2fd4e1b85edb2deb204d5e131
Daniel Panero <panero.daniel@gmail.com>
2018-08-24 17:49:51 +0200

    remove useless function template()

--- src/view/home-view.js
+++ src/view/home-view.js
@@ -13,14 +13,8 @@ import '../components/card-button'
 
 class HomeView extends LitElement {
 
-
-  async _template() {
-      return html`<card-button href="/profile/"><google-sign on-login="${({detail}) => store.dispatch(userSignedIn(detail))}}" signedInText="Go to your profile" signInText="Sign in with Google" apiKey="${GoogleApiKey}" clientId="${GoogleClientId}" scope="profile" discoveryDocs="${["https://sheets.googleapis.com/$discovery/rest?version=v4", "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"]}"></google-sign></card-button>`
-  }
-
   _render() {
-    return html`
-      ${this._template()}`
+    return html`<card-button href="/profile/"><google-sign on-login="${({detail}) => store.dispatch(userSignedIn(detail))}}" signedInText="Go to your profile" signInText="Sign in with Google" apiKey="${GoogleApiKey}" clientId="${GoogleClientId}" scope="profile" discoveryDocs="${["https://sheets.googleapis.com/$discovery/rest?version=v4", "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"]}"></google-sign></card-button>`
   }
 }
 
