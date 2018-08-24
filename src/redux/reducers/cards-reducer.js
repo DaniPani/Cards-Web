14aa93bf0e0f809215b43e58eea8f387afb1b063
Daniel Panero <panero.daniel@gmail.com>
2018-08-24 17:50:54 +0200

    when load data he overwrite a empty object

--- src/redux/reducers/cards-reducer.js
+++ src/redux/reducers/cards-reducer.js
@@ -3,10 +3,10 @@ import {
 } from '../actions/card-actions'
 
 
-export default function (state = {isLoading: true}, action) {
+export default function (state = {isLoading: true, fileSelected: false}, action) {
     switch (action.type) {
         case CARDSLOADED:
-            return {...state, ...action.payload.data, isLoading: false}
+            return {...action.payload.data, isLoading: false, fileSelected: true}
 
         case CARDSCHOSED:
                 return Object.assign(state, {fileSelected: true})
