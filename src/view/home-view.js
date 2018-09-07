6eb74fa8e6025b22216ca29c09c73698b7915fbb
Daniel Panero <panero.daniel@gmail.com>
2018-09-07 16:58:26 +0200

    cleaned home view

--- src/view/home-view.js
+++ src/view/home-view.js
@@ -1,15 +1,9 @@
 import {LitElement, html} from '@polymer/lit-element';
 
-import {GoogleApiKey, GoogleClientId} from '../../config'
-
-import '../components/google-sign'
-import '../components/card-button'
-
-
 class HomeView extends LitElement {
 
   render() {
-    return html`<card-button href="/profile/" target="_self"><google-sign signedInText="Go to your profile" signInText="Sign in with Google" apiKey=${GoogleApiKey} clientId=${GoogleClientId} scope="profile" .discoveryDocs=${["https://sheets.googleapis.com/$discovery/rest?version=v4", "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"]}></google-sign></card-button>`
+    return html``
   }
 }
 
