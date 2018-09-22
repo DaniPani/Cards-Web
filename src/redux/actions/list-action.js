b82176a55a3af150e5b4b2a9004a3fdde3bb3a5c
Daniel Panero <panero.daniel@gmail.com>
2018-09-22 20:19:34 +0200

    now folder cards rely on name

--- src/redux/actions/list-action.js
+++ src/redux/actions/list-action.js
@@ -1,14 +1,22 @@
-import{cardsFetch} from './card-actions'
+import {
+  cardsFetch
+} from './card-actions'
 
-export function listFetchDrive(folderId) {
+export function listFetchDrive(folderName) {
   return async dispatch => {
+    let responseFolder = await gapi.client.drive.files.list({
+      'q': `name = "${folderName}" and mimeType ="application/vnd.google-apps.folder"`,
+      'pageSize': 1
+    })
+    if (!responseFolder.incompleteSearch) {
       let response = await gapi.client.drive.files.list({
-          'q': `"${folderId}" in parents and mimeType = "application/vnd.google-apps.spreadsheet"`
+        'q': `"${responseFolder.result.files[0].id}" in parents and mimeType = "application/vnd.google-apps.spreadsheet"`
       })
-      if(!response.result.incompleteSearch){
+      if (!response.result.incompleteSearch) {
         await dispatch(listLoaded(response.result.files))
         response.result.files.forEach(e => dispatch(cardsFetch(e.name, e.id, 'GOOGLE')))
       }
+    }
   }
 }
 
@@ -17,6 +25,8 @@ export const LISTLOADED = 'LISTLOADED'
 export function listLoaded(list = []) {
   return {
     type: LISTLOADED,
-    payload: {list}
+    payload: {
+      list
+    }
   }
 }
